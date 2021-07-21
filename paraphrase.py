9ae869e5a0cc7922097e7ab7d7a2427e56068294
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-21 18:05:05 +0700

    commit

--- paraphrase.py
+++ paraphrase.py
@@ -2,7 +2,24 @@ import random
 
 # Dictionary of Synonyms
 synonym_dictionary = {
-    "Good": ["Nice", "Perfect"]
+    "초록색":["초록빛", "풀색", "녹색" ,"녹색 푸르른"],
+    "good": ["nice", "perfect"],
+    "많은": ["풍부한", "다양한"],
+    "너무": ["정말", "참"],
+    "살펴보도록":["알아보도록", "공부해보도록", "설명해보도록"],
+    "들어있는":["함유되어있는", "함유된", "포함되어있는"],
+    "아직":["아직까지도", "이제껏"],
+    "붉은":["빨간"],
+    "빨간":["붉은"],
+    "흐린":["구름이 낀"],
+    "날":["하루"],
+    "날에":["하루에"],
+    "날에는":["하루에는"],
+    "오늘":["지금"],
+    "오늘처럼":["지금처럼"],
+    "오늘과 같이":["지금처럼"]
+
+    
 } 
 
 
@@ -18,8 +35,8 @@ newPhrase = []
 # If input has a corresponding word in the dictionary above, substitute to synonym and add to newPharse list.
 # Otherwise, save as it is without change to newPhrase list.
 for i in words:
-    if i in synonym_dictionary:
-        newPhrase.append(random.choice(synonym_dictionary[f"{i}"]))
+    if i.lower() in synonym_dictionary:
+        newPhrase.append(random.choice(synonym_dictionary[i.lower()]))
     else: 
         newPhrase.append(i)
 
