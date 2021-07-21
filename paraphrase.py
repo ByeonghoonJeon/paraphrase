3cedecf7416131d6727002120903093474e9c748
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-21 11:58:03 +0700

    Added pseudo code

--- paraphrase.py
+++ paraphrase.py
@@ -1,22 +1,29 @@
-# Dictionary of Synonyms
 import random
 
+# Dictionary of Synonyms
 synonym_dictionary = {
-    "Hello": ["Nice", "Good"]
+    "Good": ["Nice", "Perfect"]
 } 
 
+
+# Request input
 phrase = input("Type your phrases.\n")
+
+# Split phrase to words.
 words = phrase.split()
 
+# Make empty list to save substituted words.
 newPhrase = []
 
-print(words)
-
+# If input has a corresponding word in the dictionary above, substitute to synonym and add to newPharse list.
+# Otherwise, save as it is without change to newPhrase list.
 for i in words:
     if i in synonym_dictionary:
         newPhrase.append(random.choice(synonym_dictionary[f"{i}"]))
     else: 
         newPhrase.append(i)
+
+# Make list to string and print.
 
 print (" ".join(newPhrase))
 
